1f6b09751704eb78fb3cd8755a836567c443fc52
Palominos Sylvain <sylvain.palominos@protonmail.com>
2018-11-08 12:49:26 +0100

    Fix the headers

--- main-application-api/src/main/java/org/orbisgis/mainapplicationapi/IDock.java
+++ main-application-api/src/main/java/org/orbisgis/mainapplicationapi/IDock.java
@@ -1,5 +1,5 @@
 /*
- * GIS Application is part of the OrbisGIS platform
+ * Main Application API is part of the OrbisGIS platform
  *
  * OrbisGIS is a java GIS application dedicated to research in GIScience.
  * OrbisGIS is developed by the GIS group of the DECIDE team of the
@@ -13,22 +13,22 @@
  * Institut Universitaire de Technologie de Vannes
  * 8, Rue Montaigne - BP 561 56017 Vannes Cedex
  *
- * GIS Application is distributed under GPL 3 license.
+ * Main Application API is distributed under GPL 3 license.
  *
  * Copyright (C) 2018 CNRS (Lab-STICC UMR CNRS 6285)
  *
  *
- * GIS Application is free software: you can redistribute it and/or modify it under the
+ * Main Application API is free software: you can redistribute it and/or modify it under the
  * terms of the GNU General Public License as published by the Free Software
  * Foundation, either version 3 of the License, or (at your option) any later
  * version.
  *
- * GIS Application is distributed in the hope that it will be useful, but WITHOUT ANY
+ * Main Application API is distributed in the hope that it will be useful, but WITHOUT ANY
  * WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS FOR
  * A PARTICULAR PURPOSE. See the GNU General Public License for more details.
  *
  * You should have received a copy of the GNU General Public License along with
- * GIS Application. If not, see <http://www.gnu.org/licenses/>.
+ * Main Application API. If not, see <http://www.gnu.org/licenses/>.
  *
  * For more information, please consult: <http://www.orbisgis.org/>
  * or contact directly:
